f705105687cc8550462cef3246d28d8a91b64b5c
Håkon Arneng Holmstedt <Hakon.Arneng.Holmstedt@nav.no>
2019-02-22 09:17:37 +0100

    Rewrote maksdato as a library

--- src/main/kotlin/no/nav/helse/web/ConverterResult.kt
+++
@@ -1,8 +0,0 @@
-package no.nav.helse.web
-
-import no.nav.helse.*
-
-sealed class ConverterResult
-
-data class Success(val grunnlag: Grunnlagsdata): ConverterResult()
-data class Failure(val errMsg: String): ConverterResult()
